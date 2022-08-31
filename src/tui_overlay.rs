f95e364623b06f4cc2ad5cc349f5eb3d88289101
Peter Heringer <heringer@protonmail.com>
2022-08-31 21:24:42 +0200

    Add transaction list selection and deletion in tui

--- src/tui_overlay.rs
+++ src/tui_overlay.rs
@@ -10,11 +10,11 @@ use tui::{
     layout::{Constraint, Direction, Layout},
     style::{Color, Modifier, Style},
     text::{Span, Spans},
-    widgets::{Block, Borders, ListState, List, ListItem, Row, Cell, Table, BorderType },
+    widgets::{Block, Borders, ListState, List, ListItem, Row, Cell, Table, TableState, BorderType },
     Frame, Terminal,
 };
 
-use crate::transaction;
+use crate::transaction::{self, Transaction};
 
 pub fn show_tui() -> Result<(), Box<dyn Error>> {
     match show_tui_with_io_error() {
@@ -50,8 +50,12 @@ fn show_tui_with_io_error() -> Result<(), io::Error> {
 fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
     let mut month_list_state = ListState::default();
     month_list_state.select(Some(transaction::get_months().expect("can get months").len() - 1));
+
+    let mut transaction_table_state = TableState::default();
+    transaction_table_state.select(Some(0));
+
     loop {
-        terminal.draw(|f| ui(f, &mut month_list_state))?;
+        terminal.draw(|f| ui(f, &mut month_list_state, &mut transaction_table_state))?;
 
         if let Event::Key(key) = event::read()? {
             match key.code {
@@ -78,13 +82,42 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
                         }
                     }
                 },
+                KeyCode::Char('j') => {
+                    if let Some(selected) = transaction_table_state.selected() {
+                        let amount_transactions = get_transactions_for_selected_month(&month_list_state).expect("can get transactions").len();
+                        if selected >= amount_transactions - 1 {
+                            transaction_table_state.select(Some(0))
+                        } else {
+                            transaction_table_state.select(Some(selected + 1))
+                        }
+                    }
+                },
+                KeyCode::Char('k') => {
+                    if let Some(selected) = transaction_table_state.selected() {
+                        let amount_transactions = get_transactions_for_selected_month(&month_list_state).expect("can get transactions").len();
+                        if selected > 0 {
+                            transaction_table_state.select(Some(selected - 1))
+                        } else {
+                            transaction_table_state.select(Some(amount_transactions - 1))
+                        }
+                    }
+                },
+                KeyCode::Char('d') => {
+                    let poss_month = Some(get_selected_month(&month_list_state).expect("can get month"));
+                    let selected = transaction_table_state.selected().expect("something is always selected");
+                    let amount_transactions = get_transactions_for_selected_month(&month_list_state).expect("can get transactions").len();
+                    transaction::del_entry(&poss_month, selected).expect("can delete entry");
+                    if selected == amount_transactions - 1 {
+                        transaction_table_state.select(Some(selected - 1))
+                    }
+                }
                 _ => {}
             }
         }
     }
 }
 
-fn ui<B: Backend>(f: &mut Frame<B>, month_list_state: &mut ListState) {
+fn ui<B: Backend>(f: &mut Frame<B>, month_list_state: &mut ListState, transaction_table_state: &mut TableState) {
     let chunks = Layout::default()
         .direction(Direction::Horizontal)
         .constraints(
@@ -95,12 +128,31 @@ fn ui<B: Backend>(f: &mut Frame<B>, month_list_state: &mut ListState) {
             .as_ref(),
         )
         .split(f.size());
-    let (left, right) = render_months(month_list_state);
+    let (left, right) = render_months(month_list_state, transaction_table_state);
     f.render_stateful_widget(left, chunks[0], month_list_state);
-    f.render_widget(right, chunks[1]);
+    f.render_stateful_widget(right, chunks[1], transaction_table_state);
 }
 
-fn render_months<'a>(month_list_state: &mut ListState) -> (List<'a>, Table<'a>) {
+fn get_selected_month(month_list_state: &ListState) -> Result<String, Box<dyn Error>> {
+    let month_list = transaction::get_months()?;
+    let selected_month = month_list
+        .get(
+            month_list_state
+            .selected()
+            .expect("there is always a selected month")
+            )
+        .expect("exists")
+        .clone();
+    Ok(format!("{}-01", selected_month))
+}
+
+fn get_transactions_for_selected_month(month_list_state: &ListState) -> Result<Vec<Transaction>, Box<dyn Error>> {
+    let month = get_selected_month(month_list_state)?;
+    let poss_month = Some(month);
+    transaction::get_transactions_for_month(&poss_month)
+}
+
+fn render_months<'a>(month_list_state: &mut ListState, transaction_table_state: &mut TableState) -> (List<'a>, Table<'a>) {
     let months = Block::default()
         .borders(Borders::ALL)
         .style(Style::default().fg(Color::White))
@@ -117,14 +169,6 @@ fn render_months<'a>(month_list_state: &mut ListState) -> (List<'a>, Table<'a>) 
                         )]))
         })
     .collect();
-    let selected_month = month_list
-        .get(
-            month_list_state
-            .selected()
-            .expect("there is always a selected month")
-            )
-        .expect("exists")
-        .clone();
 
     let list = List::new(items).block(months).highlight_style(
         Style::default()
@@ -134,8 +178,7 @@ fn render_months<'a>(month_list_state: &mut ListState) -> (List<'a>, Table<'a>) 
         );
 
     let mut rows: Vec<Row> = Vec::new();
-    let poss_month = Some(format!("{}-01", selected_month));
-    let month_entries = transaction::get_transactions_for_month(&poss_month).expect("can get transactions");
+    let month_entries = get_transactions_for_selected_month(month_list_state).expect("can get transaction");
 
     for transaction in month_entries {
         let row = Row::new(vec![
@@ -172,7 +215,12 @@ fn render_months<'a>(month_list_state: &mut ListState) -> (List<'a>, Table<'a>) 
                 Constraint::Percentage(20),
                 Constraint::Percentage(10),
                 Constraint::Percentage(70),
-        ]) ;
+        ])
+        .highlight_style(Style::default()
+                         .bg(Color::Yellow)
+                         .fg(Color::Black)
+                         .add_modifier(Modifier::BOLD)
+            );
 
     (list, month_detail)
 }
